9d3138b5fd8f75a9a3b6649a7ada50b8447436ac
Guillermo Blanco <guillermoechegoyenblanco@gmail.com>
2020-04-07 18:56:21 +0200

    Add search in bigrams script
    Add parameters in config file

---
+++ task_risk/config.py
@@ -0,0 +1,9 @@
+base_path = os.path.dirname(os.path.abspath(__file__))
+data_path = os.path.join(base_path, 'data/coronawhy/v6_text')
+customized_stop_words = [
+    'doi', 'preprint', 'copyright', 'peer', 'reviewed', 'org', 'https', 'et', 'al', 'author', 'figure', 
+    'rights', 'reserved', 'permission', 'used', 'using', 'biorxiv', 'fig', 'fig.', 'al.', 'q', 'license',
+    'di', 'la', 'il', 'del', 'le', 'della', 'dei', 'delle', 'una', 'da',  'dell',  'non', 'si', 'holder',
+    'p', 'h'
+]
+
--- task_risk/n_grams_dataset.py
+++ task_risk/n_grams_dataset.py
@@ -17,10 +17,7 @@ nltk.download('stopwords')
 from nltk.tokenize import word_tokenize
 
 from common.text_utils import clean_text
-
-base_path = os.path.dirname(os.path.abspath(__file__))
-data_path = os.path.join(base_path, 'data/coronawhy/v6_text')
-output_path = os.path.join(base_path, 'data/coronawhy/v6_ngrams')
+from config import base_path, data_path, customized_stop_words
 
 def process_sentence_text(stop_words, sentence_text):
     # must match sentences_df size 
@@ -55,34 +52,31 @@ def write_pickle_file(pickle_path, pickle_df):
     with gzip.open(pickle_path, 'wb') as pickle_file:
         pickle.dump(pickle_df, pickle_file)
     
-os.makedirs(output_path, exist_ok=True)
+def process_dataset():
+    output_path = os.path.join(base_path, 'data/coronawhy/v6_ngrams')
+    os.makedirs(output_path, exist_ok=True)
 
-customized_stop_words = [
-  'doi', 'preprint', 'copyright', 'peer', 'reviewed', 'org', 'https', 'et', 'al', 'author', 'figure', 
-  'rights', 'reserved', 'permission', 'used', 'using', 'biorxiv', 'fig', 'fig.', 'al.', 'q', 'license',
-  'di', 'la', 'il', 'del', 'le', 'della', 'dei', 'delle', 'una', 'da',  'dell',  'non', 'si', 'holder',
-  'p', 'h'
-]
+    stop_words = list(stopwords.words('english')) + customized_stop_words
+    pickle_filelist = glob(os.path.join(data_path, '*.pkl'))
+    errors = []
 
-stop_words = list(stopwords.words('english')) + customized_stop_words
-pickle_filelist = glob(os.path.join(data_path, '*.pkl'))
-errors = []
+    for pickle_path in pickle_filelist:
+        try:
+            pickle_df = read_pickle_file(pickle_path)
+            processed = process_pickle(stop_words, pickle_df)
+            if processed is None:
+                errors.append(str(pickle_path))
+                continue
+            pickle_filename = os.path.splitext(os.path.basename(pickle_path))[0] + '_ngrams.pkl'
+            output = os.path.join(output_path, pickle_filename)
+            write_pickle_file(output, processed)
+        except Exception as e:
+            print(f'Got exception with {pickle_path}, {e}')
 
-for pickle_path in pickle_filelist:
-    try:
-        pickle_df = read_pickle_file(pickle_path)
-        processed = process_pickle(stop_words, pickle_df)
-        if processed is None:
-            errors.append(str(pickle_path))
-            continue
-        pickle_filename = os.path.splitext(os.path.basename(pickle_path))[0] + '_ngrams.pkl'
-        output = os.path.join(output_path, pickle_filename)
-        write_pickle_file(output, processed)
-    except Exception as e:
-        print(f'Got exception with {pickle_path}, {e}')
+    print('Finish processing bi/trigrams with {} error(s)'.format(len(errors)))
+    with open(os.path.join(base_path, 'errors.json'), 'w') as json_file:
+        json_file.write(json.dumps(obj=errors) + '\n')
 
-print('Finish processing bi/trigrams with {} error(s)'.format(len(errors)))
-with open(os.path.join(base_path, 'erorrs.json'), 'w') as json_file:
-    json_file.write(json.dumps(obj=errors) + '\n')
-
+if __name__ == '__main__':
+    process_dataset()
 
